2df1525ea419f27eeae9aa041bd67aa3d2b9b2fc
Alexander Gård <alex.gaard@hotmail.com>
2021-03-01 14:40:01 +0100

    Fikset på erITestMiljo etter endring på ingress

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-const DEV_DOMAINS = ['dev', 'localhost'];
+const DEV_DOMAINS = ['dev', 'localhost', 'app-q1', 'app-q0'];
 
 export const erITestMiljo = (): boolean => {
 	return window.location.hostname.split('.').findIndex(domain => DEV_DOMAINS.includes(domain)) >= 0;
