4dbc7ec9f8cfd5fdf21544cccf88c5cc74456faa
Alexander Gård <alex.gaard@hotmail.com>
2021-05-05 13:29:14 +0200

    Legg på visning av sekunder i timestamp

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -5,5 +5,5 @@ export function formatDateTime(dateStr: string) {
 }
 
 export function toTimestamp(epochMillis: number) {
-	return dayjs(epochMillis).format('DD. MMM YYYY kl. HH:mm');
+	return dayjs(epochMillis).format('DD. MMM YYYY kl. HH:mm:ss');
 }
