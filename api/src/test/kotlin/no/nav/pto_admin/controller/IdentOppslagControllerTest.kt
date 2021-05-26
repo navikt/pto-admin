a1320ec9ac72d7fad7b87ad80aaf6f50d382d71b
Alexander Gård <alex.gaard@hotmail.com>
2021-05-26 15:10:32 +0200

    Bytt ut java tester med kotlin

--- api/src/test/kotlin/no/nav/pto_admin/controller/IdentOppslagControllerTest.kt
+++ api/src/test/kotlin/no/nav/pto_admin/controller/IdentOppslagControllerTest.kt
@@ -1,81 +1,82 @@
-package no.nav.pto_admin.controller;
+package no.nav.pto_admin.controller
 
-import no.nav.common.types.identer.AktorId;
-import no.nav.common.types.identer.Fnr;
-import no.nav.pto_admin.config.ApplicationTestConfig;
-import no.nav.pto_admin.service.IdentOppslagService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import no.nav.common.types.identer.AktorId
+import no.nav.common.types.identer.Fnr
+import no.nav.pto_admin.config.ApplicationTestConfig
+import no.nav.pto_admin.service.IdentOppslagService
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = ApplicationTestConfig.class)
-@WebMvcTest(controllers = {IdentOppslagController.class}, excludeAutoConfiguration = {OAuth2ClientAutoConfiguration.class})
-public class IdentOppslagControllerTest {
+@RunWith(SpringRunner::class)
+@ContextConfiguration(classes = [ApplicationTestConfig::class])
+@WebMvcTest(
+    controllers = [IdentOppslagController::class],
+    excludeAutoConfiguration = [OAuth2ClientAutoConfiguration::class]
+)
+class IdentOppslagControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private lateinit var mockMvc: MockMvc
 
     @MockBean
-    private IdentOppslagService identOppslagService;
+    private lateinit var identOppslagService: IdentOppslagService
 
     @Test
-    public void fnrTilAktorId__skal_sjekke_tilgang_med_parameter() throws Exception {
-        when(identOppslagService.fnrTilAktorId(any())).thenReturn(AktorId.of(""));
+    fun fnrTilAktorId__skal_sjekke_tilgang_med_parameter() {
+        whenever(identOppslagService.fnrTilAktorId(any())).thenReturn(AktorId.of(""))
 
         mockMvc.perform(
-                get("/api/ident/aktorId")
-                        .queryParam("fnr", "1234567")
-        );
+            MockMvcRequestBuilders.get("/api/ident/aktorId")
+                .queryParam("fnr", "1234567")
+        )
 
         verify(identOppslagService, times(1))
-                .fnrTilAktorId(Fnr.of("1234567"));
+            .fnrTilAktorId(Fnr.of("1234567"))
     }
 
     @Test
-    public void fnrTilAktorId__skal_returnere_har_tilgang() throws Exception {
-        when(identOppslagService.fnrTilAktorId(Fnr.of("1234567"))).thenReturn(AktorId.of("7654321"));
+    fun fnrTilAktorId__skal_returnere_har_tilgang() {
+        whenever(identOppslagService.fnrTilAktorId(Fnr.of("1234567"))).thenReturn(AktorId.of("7654321"))
 
         mockMvc.perform(
-                get("/api/ident/aktorId")
-                        .queryParam("fnr", "1234567")
-        ).andExpect(content().json("{ \"aktorId\": \"7654321\"  }"));
+            MockMvcRequestBuilders.get("/api/ident/aktorId")
+                .queryParam("fnr", "1234567")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"aktorId\": \"7654321\"  }"))
     }
 
-    // =========================
-
     @Test
-    public void aktorIdTilFnr__skal_sjekke_tilgang_med_parameter() throws Exception {
-        when(identOppslagService.aktorIdTilFnr(any())).thenReturn(Fnr.of(""));
+    fun aktorIdTilFnr__skal_sjekke_tilgang_med_parameter() {
+        whenever(identOppslagService.aktorIdTilFnr(any())).thenReturn(Fnr.of(""))
 
         mockMvc.perform(
-                get("/api/ident/fnr")
-                        .queryParam("aktorId", "7654321")
-        );
+            MockMvcRequestBuilders.get("/api/ident/fnr")
+                .queryParam("aktorId", "7654321")
+        )
 
         verify(identOppslagService, times(1))
-                .aktorIdTilFnr(AktorId.of("7654321"));
+            .aktorIdTilFnr(AktorId.of("7654321"))
     }
 
     @Test
-    public void aktorIdTilFnr__skal_returnere_har_tilgang() throws Exception {
-        when(identOppslagService.aktorIdTilFnr(AktorId.of("7654321"))).thenReturn(Fnr.of("1234567"));
+    fun aktorIdTilFnr__skal_returnere_har_tilgang() {
+        whenever(identOppslagService.aktorIdTilFnr(AktorId.of("7654321"))).thenReturn(Fnr.of("1234567"))
 
         mockMvc.perform(
-                get("/api/ident/fnr")
-                        .queryParam("aktorId", "7654321")
-        ).andExpect(content().json("{ \"fnr\": \"1234567\"  }"));
+            MockMvcRequestBuilders.get("/api/ident/fnr")
+                .queryParam("aktorId", "7654321")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"fnr\": \"1234567\"  }"))
     }
-
 }
--- api/src/test/kotlin/no/nav/pto_admin/controller/TilgangOppslagControllerTest.kt
+++ api/src/test/kotlin/no/nav/pto_admin/controller/TilgangOppslagControllerTest.kt
@@ -1,176 +1,172 @@
-package no.nav.pto_admin.controller;
+package no.nav.pto_admin.controller
 
-import no.nav.common.types.identer.EnhetId;
-import no.nav.common.types.identer.NavIdent;
-import no.nav.common.types.identer.NorskIdent;
-import no.nav.pto_admin.config.ApplicationTestConfig;
-import no.nav.pto_admin.service.TilgangOppslagService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import no.nav.common.types.identer.EnhetId
+import no.nav.common.types.identer.NavIdent
+import no.nav.common.types.identer.NorskIdent
+import no.nav.pto_admin.config.ApplicationTestConfig
+import no.nav.pto_admin.service.TilgangOppslagService
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = ApplicationTestConfig.class)
-@WebMvcTest(controllers = {TilgangOppslagController.class}, excludeAutoConfiguration = {OAuth2ClientAutoConfiguration.class})
-public class TilgangOppslagControllerTest {
+@RunWith(SpringRunner::class)
+@ContextConfiguration(classes = [ApplicationTestConfig::class])
+@WebMvcTest(
+    controllers = [TilgangOppslagController::class],
+    excludeAutoConfiguration = [OAuth2ClientAutoConfiguration::class]
+)
+class TilgangOppslagControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private lateinit var mockMvc: MockMvc
 
     @MockBean
-    private TilgangOppslagService tilgangOppslagService;
+    private lateinit var tilgangOppslagService: TilgangOppslagService
 
     @Test
-    public void harTilgangTilEnhet__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harTilgangTilEnhet__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/enhet")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("enhetId", "1234")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/enhet")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("enhetId", "1234")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harTilgangTilEnhet(NavIdent.of("Z1234"), EnhetId.of("1234"));
+            .harTilgangTilEnhet(NavIdent.of("Z1234"), EnhetId.of("1234"))
     }
 
     @Test
-    public void harTilgangTilEnhet__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harTilgangTilEnhet(any(), any())).thenReturn(true);
+    fun harTilgangTilEnhet__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harTilgangTilEnhet(any(), any()))
+            .thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/enhet")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("enhetId", "1234")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/enhet")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("enhetId", "1234")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
 
-    // =========================
-
     @Test
-    public void harSkrivetilgang__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harSkrivetilgang__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/skriv")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("norskIdent", "1234567")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/skriv")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("norskIdent", "1234567")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harSkrivetilgang(NavIdent.of("Z1234"), NorskIdent.of("1234567"));
+            .harSkrivetilgang(NavIdent.of("Z1234"), NorskIdent.of("1234567"))
     }
 
     @Test
-    public void harSkrivetilgang__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harSkrivetilgang(any(), any())).thenReturn(true);
+    fun harSkrivetilgang__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harSkrivetilgang(any(), any()))
+            .thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/skriv")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("norskIdent", "1234567")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/skriv")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("norskIdent", "1234567")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
 
-    // =========================
-
     @Test
-    public void harLesetilgang__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harLesetilgang__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/les")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("norskIdent", "1234567")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/les")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("norskIdent", "1234567")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harLesetilgang(NavIdent.of("Z1234"), NorskIdent.of("1234567"));
+            .harLesetilgang(NavIdent.of("Z1234"), NorskIdent.of("1234567"))
     }
 
     @Test
-    public void harLesetilgang__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harLesetilgang(any(), any())).thenReturn(true);
+    fun harLesetilgang__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harLesetilgang(any(), any()))
+            .thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/les")
-                        .queryParam("navIdent", "Z1234")
-                        .queryParam("norskIdent", "1234567")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/les")
+                .queryParam("navIdent", "Z1234")
+                .queryParam("norskIdent", "1234567")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
 
-    // =========================
-
     @Test
-    public void harTilgangTilKode6__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harTilgangTilKode6__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/kode6")
-                        .queryParam("navIdent", "Z1234")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/kode6")
+                .queryParam("navIdent", "Z1234")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harTilgangTilKode6(NavIdent.of("Z1234"));
+            .harTilgangTilKode6(NavIdent.of("Z1234"))
     }
 
     @Test
-    public void harTilgangTilKode6__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harTilgangTilKode6(any())).thenReturn(true);
+    fun harTilgangTilKode6__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harTilgangTilKode6(any())).thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/kode6")
-                        .queryParam("navIdent", "Z1234")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/kode6")
+                .queryParam("navIdent", "Z1234")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
 
-    // =========================
-
     @Test
-    public void harTilgangTilKode7__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harTilgangTilKode7__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/kode7")
-                        .queryParam("navIdent", "Z1234")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/kode7")
+                .queryParam("navIdent", "Z1234")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harTilgangTilKode7(NavIdent.of("Z1234"));
+            .harTilgangTilKode7(NavIdent.of("Z1234"))
     }
 
     @Test
-    public void harTilgangTilKode7__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harTilgangTilKode7(any())).thenReturn(true);
+    fun harTilgangTilKode7__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harTilgangTilKode7(any())).thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/kode7")
-                        .queryParam("navIdent", "Z1234")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/kode7")
+                .queryParam("navIdent", "Z1234")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
 
-    // =========================
-
     @Test
-    public void harTilgangTilSkjermetPerson__skal_sjekke_tilgang_med_parameter() throws Exception {
+    fun harTilgangTilSkjermetPerson__skal_sjekke_tilgang_med_parameter() {
         mockMvc.perform(
-                get("/api/tilgang/skjermet")
-                        .queryParam("navIdent", "Z1234")
-        );
+            MockMvcRequestBuilders.get("/api/tilgang/skjermet")
+                .queryParam("navIdent", "Z1234")
+        )
 
         verify(tilgangOppslagService, times(1))
-                .harTilgangTilSkjermetPerson(NavIdent.of("Z1234"));
+            .harTilgangTilSkjermetPerson(NavIdent.of("Z1234"))
     }
 
     @Test
-    public void harTilgangTilSkjermetPerson__skal_returnere_har_tilgang() throws Exception {
-        when(tilgangOppslagService.harTilgangTilSkjermetPerson(any())).thenReturn(true);
+    fun harTilgangTilSkjermetPerson__skal_returnere_har_tilgang() {
+        whenever(tilgangOppslagService.harTilgangTilSkjermetPerson(any())).thenReturn(true)
 
         mockMvc.perform(
-                get("/api/tilgang/skjermet")
-                        .queryParam("navIdent", "Z1234")
-        ).andExpect(content().json("{ \"harTilgang\": true  }"));
+            MockMvcRequestBuilders.get("/api/tilgang/skjermet")
+                .queryParam("navIdent", "Z1234")
+        ).andExpect(MockMvcResultMatchers.content().json("{ \"harTilgang\": true  }"))
     }
-
 }
