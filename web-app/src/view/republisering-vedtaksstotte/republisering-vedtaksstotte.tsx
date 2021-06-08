184e18d021a336371ad8eac5be6d9f99c452decc
matiasvinjevoll <matias.vinjevoll@nav.no>
2021-06-08 12:44:42 +0200

    Vise jobId i kort i stede for toast

--- web-app/src/view/republisering-vedtaksstotte/republisering-vedtaksstotte.tsx
+++ web-app/src/view/republisering-vedtaksstotte/republisering-vedtaksstotte.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../../component/card/card';
 import { Flatknapp } from 'nav-frontend-knapper';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { republiserInnsatsbehovVedtaksstotte } from '../../api';
-import { errorToast } from '../../utils/toast-utils';
+import { errorToast, successToast } from '../../utils/toast-utils';
 import './republisering-vedtaksstotte.less';
-import { toast } from 'react-toastify';
 
 export function RepubliseringVedtaksstotte() {
+
+	const [jobId, setJobId] = useState<string | undefined>(undefined)
 
 	function handleRepubliserInnsatsbehovVedtaksstotte() {
 		republiserInnsatsbehovVedtaksstotte()
 			.then((resp) => {
-				toast(`Republisering av innsatsbehov er startet med jobId ${resp.data}`, {
-					type: 'success',
-					position: 'top-right',
-					autoClose: false,
-					closeOnClick: false
-				});
+				setJobId(resp.data)
+				successToast(`Republisering av innsatsbehov er startet`);
 			})
 			.catch(() => errorToast('Klarte ikke å starte republisering av innsatsbehov'));
 	}
@@ -31,6 +28,9 @@ export function RepubliseringVedtaksstotte() {
 					for brukere som bare har vedtak i Arena, men dersom en bruker har vedtak i Modia vedtaksstøtte og et
 					nyere i Arena, så vil innsatsbehovet fra vedtak i Arena bli republisert.
 				</Normaltekst>
+				{jobId && <Normaltekst className="blokk-xxs">
+					Jobb startet med jobId: {jobId}
+				</Normaltekst>}
 				<Flatknapp onClick={handleRepubliserInnsatsbehovVedtaksstotte}>Republiser innsatsbehov</Flatknapp>
 			</Card>
 		</div>
