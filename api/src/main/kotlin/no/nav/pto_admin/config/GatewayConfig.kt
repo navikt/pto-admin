88f69115d06c6fd1de206da4c60abe9c53c4d01a
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-05-19 13:32:57 +0200

    logger proxy-kall

--- api/src/main/kotlin/no/nav/pto_admin/config/GatewayConfig.kt
+++ api/src/main/kotlin/no/nav/pto_admin/config/GatewayConfig.kt
@@ -5,6 +5,8 @@ import no.nav.common.log.LogFilter.NAV_CALL_ID_HEADER_NAMES
 import no.nav.common.rest.client.RestUtils
 import no.nav.common.sts.SystemUserTokenProvider
 import no.nav.common.utils.IdUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.gateway.filter.GatewayFilterChain
 import org.springframework.cloud.gateway.filter.GlobalFilter
@@ -12,30 +14,42 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 
 @Configuration
 class GatewayConfig {
 
+
     @Autowired
     lateinit var systemUserTokenProvider: SystemUserTokenProvider
 
     @Bean
-    fun customGlobalFilter(): GlobalFilter? {
-        return GlobalFilter { exchange: ServerWebExchange, chain: GatewayFilterChain ->
+    fun customGlobalFilter(): GlobalFilter {
 
-            exchange.request.mutate()
-                .header(HttpHeaders.AUTHORIZATION, RestUtils.createBearerToken(systemUserTokenProvider.systemUserToken))
-                .build()
+        return object : GlobalFilter {
+            val log: Logger = LoggerFactory.getLogger(GlobalFilter::class.java)
 
-            val callId = NAV_CALL_ID_HEADER_NAMES.flatMap { exchange.request.headers[it] ?: emptyList() }.find { true }
-                ?: IdUtils.generateId()
+            override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
+                exchange.request.mutate()
+                    .header(
+                        HttpHeaders.AUTHORIZATION,
+                        RestUtils.createBearerToken(systemUserTokenProvider.systemUserToken)
+                    )
+                    .build()
 
-            if (callId != null) {
-                exchange.request.mutate().header(LogFilter.PREFERRED_NAV_CALL_ID_HEADER_NAME, callId).build()
+                val callId =
+                    NAV_CALL_ID_HEADER_NAMES.flatMap { exchange.request.headers[it] ?: emptyList() }.find { true }
+                        ?: IdUtils.generateId()
+
+                if (callId != null) {
+                    exchange.request.mutate().header(LogFilter.PREFERRED_NAV_CALL_ID_HEADER_NAME, callId).build()
+                }
+
+                log.info("Proxyer request til " + exchange.request.remoteAddress + exchange.request.path)
+
+                return chain.filter(exchange)
             }
-
-            chain.filter(exchange)
         }
     }
 }
