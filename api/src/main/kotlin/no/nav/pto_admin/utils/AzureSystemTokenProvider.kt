4e1d85976169c04378e6230cc5ed20a57c26f089
stianStensli <stian.horn.stensli@nav.no>
2022-09-02 13:01:19 +0200

    Bruker AzureAd system tokens for veilarbportefolje

---
+++ api/src/main/kotlin/no/nav/pto_admin/utils/AzureSystemTokenProvider.kt
@@ -0,0 +1,9 @@
+package no.nav.pto_admin.utils
+
+class AzureSystemTokenProvider(private val systemTokenProviders: Map<SystembrukereAzure, () -> String>) {
+
+    fun getSystemToken(system: SystembrukereAzure): (() -> String)? {
+        return systemTokenProviders[system]
+    }
+
+}
---
+++ api/src/main/kotlin/no/nav/pto_admin/utils/SystembrukereAzure.kt
@@ -0,0 +1,5 @@
+package no.nav.pto_admin.utils
+
+enum class SystembrukereAzure {
+    VEILARBPORTEFOLJE
+}
