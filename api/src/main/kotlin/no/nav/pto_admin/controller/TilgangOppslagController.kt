72716431a75bd14d24f32114c3808206e1a09a6b
JulieHillRoa <julie.hill.roa@nav.no>
2023-03-29 15:27:45 +0200

    Erstatter abac med poao-tilgang

--- api/src/main/kotlin/no/nav/pto_admin/controller/TilgangOppslagController.kt
+++ api/src/main/kotlin/no/nav/pto_admin/controller/TilgangOppslagController.kt
@@ -8,45 +8,58 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api/tilgang")
 class TilgangOppslagController(private val tilgangOppslagService: TilgangOppslagService) {
 
     @GetMapping("/enhet")
-    fun harTilgangTilEnhet(@RequestParam navIdent: NavIdent, @RequestParam enhetId: EnhetId): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harTilgangTilEnhet(navIdent, enhetId))
+    fun harTilgangTilEnhet(@RequestParam navIdent: NavIdent, @RequestParam enhetId: EnhetId): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harTilgangTilEnhet(navIdent, enhetId).map {
+            HarTilgangResponse(it)
+        }
     }
 
     @GetMapping("/skriv")
     fun harSkrivetilgang(
         @RequestParam navIdent: NavIdent,
         @RequestParam norskIdent: NorskIdent
-    ): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harSkrivetilgang(navIdent, norskIdent))
+    ): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harSkrivetilgang(navIdent, norskIdent).map {
+            HarTilgangResponse(it)
+        }
     }
 
     @GetMapping("/les")
     fun harLesetilgang(
         @RequestParam navIdent: NavIdent,
         @RequestParam norskIdent: NorskIdent
-    ): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harLesetilgang(navIdent, norskIdent))
+    ): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harLesetilgang(navIdent, norskIdent).map {
+            HarTilgangResponse(it)
+        }
     }
 
     @GetMapping("/kode6")
-    fun harTilgangTilKode6(@RequestParam navIdent: NavIdent): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harTilgangTilKode6(navIdent))
+    fun harTilgangTilKode6(@RequestParam navIdent: NavIdent): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harTilgangTilKode6(navIdent).map {
+            HarTilgangResponse(it)
+        }
     }
 
     @GetMapping("/kode7")
-    fun harTilgangTilKode7(@RequestParam navIdent: NavIdent): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harTilgangTilKode7(navIdent))
+    fun harTilgangTilKode7(@RequestParam navIdent: NavIdent): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harTilgangTilKode7(navIdent).map {
+            HarTilgangResponse(it)
+        }
     }
 
     @GetMapping("/skjermet")
-    fun harTilgangTilSkjermetPerson(@RequestParam navIdent: NavIdent): HarTilgangResponse {
-        return HarTilgangResponse(tilgangOppslagService.harTilgangTilSkjermetPerson(navIdent))
+    fun harTilgangTilSkjermetPerson(@RequestParam navIdent: NavIdent): Mono<HarTilgangResponse> {
+        return tilgangOppslagService.harTilgangTilSkjermetPerson(navIdent).map {
+            HarTilgangResponse(it)
+        }
     }
 
     data class HarTilgangResponse(val harTilgang: Boolean)
--- api/src/main/kotlin/no/nav/pto_admin/service/TilgangOppslagService.kt
+++ api/src/main/kotlin/no/nav/pto_admin/service/TilgangOppslagService.kt
@@ -6,33 +6,113 @@ import no.nav.common.types.identer.EnhetId
 import no.nav.common.types.identer.Fnr
 import no.nav.common.types.identer.NavIdent
 import no.nav.common.types.identer.NorskIdent
+import no.nav.poao_tilgang.client.NavAnsattBehandleFortroligBrukerePolicyInput
+import no.nav.poao_tilgang.client.NavAnsattBehandleSkjermedePersonerPolicyInput
+import no.nav.poao_tilgang.client.NavAnsattBehandleStrengtFortroligBrukerePolicyInput
+import no.nav.poao_tilgang.client.NavAnsattTilgangTilEksternBrukerPolicyInput
+import no.nav.poao_tilgang.client.NavAnsattTilgangTilNavEnhetPolicyInput
+import no.nav.poao_tilgang.client.PoaoTilgangClient
+import no.nav.poao_tilgang.client.TilgangType
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import reactor.core.publisher.Mono
+import java.util.UUID
 
 @Service
-class TilgangOppslagService(val veilarbPep: Pep) {
+class TilgangOppslagService(val veilarbPep: Pep, val poaoTilgangClient: PoaoTilgangClient, val unleashService: UnleashService, val authService: AuthService) {
 
-    fun harTilgangTilEnhet(navIdent: NavIdent, enhetId: EnhetId): Boolean {
-        return veilarbPep.harVeilederTilgangTilEnhet(navIdent, enhetId)
+    fun harTilgangTilEnhet(navIdent: NavIdent, enhetId: EnhetId): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			 return authService.hentInnloggetBrukerAzureId().map {
+				 it?.let {
+					 val decision =
+						 poaoTilgangClient.evaluatePolicy(NavAnsattTilgangTilNavEnhetPolicyInput(it, enhetId.get()))
+							 .getOrThrow()
+					 decision.isPermit
+				 } ?: false
+			 }
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilEnhet(navIdent, enhetId))
     }
 
-    fun harLesetilgang(navIdent: NavIdent, norskIdent: NorskIdent): Boolean {
-        return veilarbPep.harVeilederTilgangTilPerson(navIdent, ActionId.READ, Fnr.of(norskIdent.get()))
+    fun harLesetilgang(navIdent: NavIdent, norskIdent: NorskIdent): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			return authService.hentInnloggetBrukerAzureId().map {
+				it?.let {
+					val decision = poaoTilgangClient.evaluatePolicy(NavAnsattTilgangTilEksternBrukerPolicyInput(it , TilgangType.LESE, norskIdent.get())).getOrThrow()
+					 decision.isPermit
+				} ?: false
+			}
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilPerson(navIdent, ActionId.READ, Fnr.of(norskIdent.get())))
     }
 
-    fun harSkrivetilgang(navIdent: NavIdent, norskIdent: NorskIdent): Boolean {
-        return veilarbPep.harVeilederTilgangTilPerson(navIdent, ActionId.WRITE, Fnr.of(norskIdent.get()))
+    fun harSkrivetilgang(navIdent: NavIdent, norskIdent: NorskIdent): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			return authService.hentInnloggetBrukerAzureId().map {
+				it?.let {
+					val decision = poaoTilgangClient.evaluatePolicy(
+						NavAnsattTilgangTilEksternBrukerPolicyInput(
+							it,
+							TilgangType.SKRIVE,
+							norskIdent.get()
+						)
+					).getOrThrow()
+					decision.isPermit
+				} ?: false
+			}
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilPerson(navIdent, ActionId.WRITE, Fnr.of(norskIdent.get())))
     }
 
-    fun harTilgangTilKode6(navIdent: NavIdent): Boolean {
-        return veilarbPep.harVeilederTilgangTilKode6(navIdent)
+    fun harTilgangTilKode6(navIdent: NavIdent): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			return authService.hentInnloggetBrukerAzureId().map {
+				it?.let {
+					val decision = poaoTilgangClient.evaluatePolicy(
+						NavAnsattBehandleStrengtFortroligBrukerePolicyInput(
+							it
+						)
+					).getOrThrow()
+					decision.isPermit
+				} ?: false
+			}
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilKode6(navIdent))
     }
 
-    fun harTilgangTilKode7(navIdent: NavIdent): Boolean {
-        return veilarbPep.harVeilederTilgangTilKode7(navIdent)
+    fun harTilgangTilKode7(navIdent: NavIdent): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			if (unleashService.skalBrukePoaoTilgang()) {
+				return authService.hentInnloggetBrukerAzureId().map {
+					it?.let {
+						val decision = poaoTilgangClient.evaluatePolicy(
+							NavAnsattBehandleFortroligBrukerePolicyInput(
+								it
+							)
+						).getOrThrow()
+						decision.isPermit
+					} ?: false
+				}
+			}
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilKode7(navIdent))
     }
 
-    fun harTilgangTilSkjermetPerson(navIdent: NavIdent): Boolean {
-        return veilarbPep.harVeilederTilgangTilEgenAnsatt(navIdent)
+    fun harTilgangTilSkjermetPerson(navIdent: NavIdent): Mono<Boolean> {
+		if (unleashService.skalBrukePoaoTilgang()) {
+			return authService.hentInnloggetBrukerAzureId().map {
+				it?.let {
+					val decision = poaoTilgangClient.evaluatePolicy(
+						NavAnsattBehandleSkjermedePersonerPolicyInput(
+							it
+						)
+					).getOrThrow()
+					decision.isPermit
+				} ?: false
+			}
+		}
+        return Mono.just(veilarbPep.harVeilederTilgangTilEgenAnsatt(navIdent))
     }
-
 }
