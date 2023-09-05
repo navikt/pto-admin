e2024d09e54b0b7b635444a6c15f13c2725d4c4b
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-09-05 18:14:39 +0200

    Try to adjust WebSecurityConfig to Spring Security 6

--- api/src/main/kotlin/no/nav/pto_admin/config/WebSecurityConfig.kt
+++ api/src/main/kotlin/no/nav/pto_admin/config/WebSecurityConfig.kt
@@ -1,29 +1,27 @@
 package no.nav.pto_admin.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod.GET
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher
 
 
 @EnableWebFluxSecurity
 @Configuration
 class WebSecurityConfig {
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-        http
-            .authorizeExchange { authorize: AuthorizeExchangeSpec ->
+    fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.authorizeExchange { authorize: AuthorizeExchangeSpec ->
                 authorize
                     .pathMatchers(GET, "/internal/**").permitAll()
                     .pathMatchers(GET, "/oauth2/**").permitAll()
                     .anyExchange().authenticated()
             }
-            .oauth2Login().authenticationMatcher(PathPatternParserServerWebExchangeMatcher("/oauth2/callback"))
-            .and()
-            .csrf().disable() // session cookie er SameSite lax
+            .oauth2Login {}
+            .csrf{csrf -> csrf.disable()}
         return http.build()
     }
 }
