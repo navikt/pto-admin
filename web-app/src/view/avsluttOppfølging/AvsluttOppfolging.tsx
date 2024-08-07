4ef7d3c84967a29e9dd2d2c5fc22809748bc58bc
sigurdgroneng <sigurd.groneng@nav.no>
2024-08-07 14:24:55 +0200

    Design forms

--- web-app/src/view/avsluttOppfølging/AvsluttOppfolging.tsx
+++ web-app/src/view/avsluttOppfølging/AvsluttOppfolging.tsx
@@ -1,23 +1,37 @@
 import { batchAvsluttOppfolging } from '../../api';
+import {useState} from "react";
 export const AvsluttOppfolging = () => {
 
-    function handleSubmit(e: any) {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(undefined)
+
+    async function handleSubmit(e: any) {
         e.preventDefault();
         const {aktorIds: aktorIdStrings, begrunnelse} = Object.fromEntries(new FormData(e.target)) as unknown as {aktorIds: string, begrunnelse: string}
         const aktorIds = aktorIdStrings.split(',').map(aktorId => aktorId.trim())
-        // tslint:disable-next-line:no-console
-        batchAvsluttOppfolging({aktorIds, begrunnelse})
+        try {
+            setError(undefined)
+            setIsLoading(true)
+            await batchAvsluttOppfolging({aktorIds, begrunnelse})
+        } catch (e: any) {
+            setError(e?.toString)
+        } finally {
+            setIsLoading(false)
+        }
     }
+
+    console.log({ isLoading })
+
     return <div className="view kafka-admin">
         <form onSubmit={
             handleSubmit
-        }>
+        } className="large-card card__content space-y-4" style={{ display: 'flex' }}>
             <h1>Avslutt oppfølging for mange brukere</h1>
             <label htmlFor="brukere" >Liste over brukere som skal avsluttes:</label>
-            <textarea id="brukere" name="aktorIds" />
+            <textarea id="brukere" name="aktorIds" disabled={isLoading} />
             <label htmlFor="begrunnelse">Begrunnelse for avsluttning av oppfølging:</label>
-            <input id="begrunnelse" type="text" name="begrunnelse"/>
-            <input type="submit" />
+            <input id="begrunnelse" type="text" name="begrunnelse" disabled={isLoading}/>
+            <input type="submit" disabled={isLoading} />
         </form>
     </div>
 }
