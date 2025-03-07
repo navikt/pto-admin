a824a03768209a261d334fbe7b795ba71c8abbf9
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-07 10:23:40 +0100

    Use OBO token exchange for veilarboppfolging

--- api/src/main/kotlin/no/nav/pto_admin/config/ApplicationConfig.kt
+++ api/src/main/kotlin/no/nav/pto_admin/config/ApplicationConfig.kt
@@ -14,6 +14,7 @@ import no.nav.common.utils.NaisUtils
 import no.nav.common.utils.UrlUtils
 import no.nav.common.token_client.builder.AzureAdTokenClientBuilder
 import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient
+import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient
 import no.nav.poao_tilgang.client.PoaoTilgangCachedClient
 import no.nav.poao_tilgang.client.PoaoTilgangClient
 import no.nav.poao_tilgang.client.PoaoTilgangHttpClient
@@ -54,8 +55,15 @@ class ApplicationConfig {
     }
 
     @Bean
-    fun azureSystemTokenProvider(tokenClient: AzureAdMachineToMachineTokenClient): AzureSystemTokenProvider {
-        val veilarbportefoljeTokenProvider: () -> String = {
+    fun azureAdOboTokenClient(): AzureAdOnBehalfOfTokenClient {
+        return AzureAdTokenClientBuilder.builder()
+            .withNaisDefaults()
+            .buildOnBehalfOfTokenClient()
+    }
+
+    @Bean
+    fun azureSystemTokenProvider(tokenClient: AzureAdMachineToMachineTokenClient, oboClient: AzureAdOnBehalfOfTokenClient): AzureSystemTokenProvider {
+        val veilarbportefoljeTokenProvider: (token: String) -> String = {
             tokenClient.createMachineToMachineToken(
                 String.format(
                     "api://%s-gcp.obo.veilarbportefolje/.default",
@@ -63,7 +71,7 @@ class ApplicationConfig {
                 )
             )
         }
-        val veilarbvedtaksstotteTokenProvider: () -> String = {
+        val veilarbvedtaksstotteTokenProvider: (token: String) -> String = {
             tokenClient.createMachineToMachineToken(
                 String.format(
                     "api://%s-gcp.obo.veilarbvedtaksstotte/.default",
@@ -71,16 +79,14 @@ class ApplicationConfig {
                 )
             )
         }
-        val veilarboppfolgingTokenProvider: () -> String = {
-            tokenClient.createMachineToMachineToken(
-                String.format(
-                    "api://%s-gcp.poao.veilarboppfolging/.default",
-                    if (EnvironmentUtils.isProduction().orElseThrow()) "prod" else "dev"
-                )
-            )
+        val veilarboppfolgingTokenProvider: (token: String) -> String = { token ->
+            oboClient.exchangeOnBehalfOfToken(String.format(
+                "api://%s-gcp.poao.veilarboppfolging/.default",
+                if (EnvironmentUtils.isProduction().orElseThrow()) "prod" else "dev"
+            ), token)
         }
 
-        val systemTokenSuppliers: Map<SystembrukereAzure, () -> String> =
+        val systemTokenSuppliers: Map<SystembrukereAzure, (String) -> String> =
             mapOf(SystembrukereAzure.VEILARBPORTEFOLJE to veilarbportefoljeTokenProvider,
                 SystembrukereAzure.VEILARBOPPFOLGING to veilarboppfolgingTokenProvider,
                 SystembrukereAzure.VEILARBVEDTAKSTOTTE to veilarbvedtaksstotteTokenProvider)
--- api/src/main/kotlin/no/nav/pto_admin/utils/AzureSystemTokenProvider.kt
+++ api/src/main/kotlin/no/nav/pto_admin/utils/AzureSystemTokenProvider.kt
@@ -1,9 +1,12 @@
 package no.nav.pto_admin.utils
 
-class AzureSystemTokenProvider(private val systemTokenProviders: Map<SystembrukereAzure, () -> String>) {
+class AzureSystemTokenProvider(private val systemTokenProviders: Map<SystembrukereAzure, (token: String) -> String>) {
 
     fun getSystemToken(system: SystembrukereAzure): (String) {
-        return systemTokenProviders[system]!!.invoke()
+        return systemTokenProviders[system]!!.invoke("unused")
     }
 
+    fun getOboToken(system: SystembrukereAzure, token: String): (String) {
+        return systemTokenProviders[system]!!.invoke(token)
+    }
 }
