1ff1abab49deee41e331220260437db2592b5aaa
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-11 07:53:01 +0100

    Log when obo exchange was ok

--- api/src/main/kotlin/no/nav/pto_admin/utils/AzureSystemTokenProvider.kt
+++ api/src/main/kotlin/no/nav/pto_admin/utils/AzureSystemTokenProvider.kt
@@ -1,12 +1,23 @@
 package no.nav.pto_admin.utils
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
 class AzureSystemTokenProvider(private val systemTokenProviders: Map<SystembrukereAzure, (token: String) -> String>) {
+    val log: Logger = LoggerFactory.getLogger(AzureSystemTokenProvider::class.java)
 
     fun getSystemToken(system: SystembrukereAzure): (String) {
         return systemTokenProviders[system]!!.invoke("unused")
     }
 
     fun getOboToken(system: SystembrukereAzure, token: String): (String) {
-        return systemTokenProviders[system]!!.invoke(token)
+        try {
+            val token = systemTokenProviders[system]!!.invoke(token)
+            log.info("obo exchange ok - $system")
+            return token
+        } catch (e: Exception) {
+            throw RuntimeException("Feil ved henting av system token", e)
+        }
     }
 }
