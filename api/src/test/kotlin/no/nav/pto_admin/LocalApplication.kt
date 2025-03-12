6df23d9c95ccac3933b4aaeebe8575f8487d2c07
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-12 07:36:26 +0100

    Remove unsued import

--- api/src/test/kotlin/no/nav/pto_admin/LocalApplication.kt
+++ api/src/test/kotlin/no/nav/pto_admin/LocalApplication.kt
@@ -1,7 +1,6 @@
 package no.nav.pto_admin
 
 import no.nav.pto_admin.config.ApplicationTestConfig
-import no.nav.pto_admin.config.ClientHttpConnectorConfig
 import no.nav.pto_admin.config.SetupLocalEnvironment
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
@@ -9,7 +8,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @EnableAutoConfiguration(exclude = [ReactiveUserDetailsServiceAutoConfiguration::class])
-@Import(value = [ApplicationTestConfig::class, ClientHttpConnectorConfig::class])
+@Import(value = [ApplicationTestConfig::class])
 class LocalApplication
 
 fun main(args: Array<String>) {
