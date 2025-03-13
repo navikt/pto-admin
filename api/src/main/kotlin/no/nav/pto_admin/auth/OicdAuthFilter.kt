225d60269b5a23146978b15d06d165a1482a9014
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-13 06:56:31 +0100

    Rewrite auth filter to spring webflux

---
+++ api/src/main/kotlin/no/nav/pto_admin/auth/OicdAuthFilter.kt
@@ -0,0 +1,90 @@
+package no.nav.pto_admin.auth
+
+import com.nimbusds.jose.JOSEException
+import com.nimbusds.jose.proc.BadJOSEException
+import com.nimbusds.jwt.JWTParser
+import com.nimbusds.openid.connect.sdk.validators.BadJWTExceptions
+import no.nav.common.auth.context.AuthContext
+import no.nav.common.auth.context.AuthContextHolderThreadLocal
+import no.nav.common.auth.oidc.UserRoleNullException
+import no.nav.common.auth.utils.TokenUtils
+import no.nav.common.utils.fn.UnsafeRunnable
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+import java.text.ParseException
+
+class OicdAuthFilter(private val oidcAuthenticators: List<OidcAuthenticator>): WebFilter {
+
+    override fun filter(
+        exchange: ServerWebExchange,
+        chain: WebFilterChain
+    ): Mono<Void> {
+
+        val request = exchange.request
+        val response = exchange.response
+
+        for (authenticator in oidcAuthenticators) {
+            val token = authenticator.findIdToken(request)
+
+            if (token != null) {
+                try {
+                    var jwtToken = JWTParser.parse(token)
+
+                    // Skip this authenticator if the audience is not matching
+                    if (!TokenUtils.hasMatchingAudience(jwtToken, authenticator.config.clientIds)) {
+                        continue
+                    }
+
+                    authenticator.tokenValidator.validate(jwtToken)
+
+                    val userRole = authenticator.config.userRoleResolver.resolve(jwtToken.jwtClaimsSet)
+                    if (userRole == null) {
+                        throw UserRoleNullException()
+                    }
+
+                    val authContext = AuthContext(userRole, jwtToken)
+
+                    AuthContextHolderThreadLocal.instance()
+                        .withContext(authContext, UnsafeRunnable { chain.filter(exchange) })
+
+                    return Mono.empty()
+                } catch (exception: ParseException) {
+                    if (exception === BadJWTExceptions.EXPIRED_EXCEPTION) {
+                        logger.info("Token validation failed", exception)
+                    } else {
+                        logger.error("Token validation failed", exception)
+                    }
+                } catch (exception: JOSEException) {
+                    if (exception === BadJWTExceptions.EXPIRED_EXCEPTION) {
+                        logger.info("Token validation failed", exception)
+                    } else {
+                        logger.error("Token validation failed", exception)
+                    }
+                } catch (exception: BadJOSEException) {
+                    if (exception === BadJWTExceptions.EXPIRED_EXCEPTION) {
+                        logger.info("Token validation failed", exception)
+                    } else {
+                        logger.error("Token validation failed", exception)
+                    }
+                } catch (e: UserRoleNullException) {
+                    logger.error("User roll is null")
+                }
+            }
+        }
+
+        response.statusCode = HttpStatus.UNAUTHORIZED
+        return Mono.empty()
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(OicdAuthFilter::class.java)
+
+        // Check if the token is about to expire within the next 5 minutes
+        private val CHECK_EXPIRES_WITHIN = (1000 * 60 * 5).toLong()
+    }
+}
--- api/src/main/kotlin/no/nav/pto_admin/config/FilterConfig.kt
+++ api/src/main/kotlin/no/nav/pto_admin/config/FilterConfig.kt
@@ -1,78 +1,31 @@
 package no.nav.pto_admin.config
 
-import jakarta.servlet.Filter
-import jakarta.servlet.ServletException
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import no.nav.common.auth.oidc.filter.AzureAdUserRoleResolver
-import no.nav.common.auth.oidc.filter.OidcAuthenticationFilter
-import no.nav.common.auth.oidc.filter.OidcAuthenticator.fromConfigs
-import no.nav.common.auth.oidc.filter.OidcAuthenticatorConfig
+import no.nav.pto_admin.auth.OicdAuthFilter
+import no.nav.pto_admin.auth.OidcAuthenticator
+import no.nav.pto_admin.auth.OidcAuthenticatorConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.server.reactive.ServerHttpRequest
-import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.reactive.config.WebFluxConfigurer
-import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
-import reactor.core.publisher.Mono
-import reactor.core.publisher.MonoSink
-import java.io.IOException
-import java.util.function.Consumer
 
 
 @Configuration
 class FilterConfig: WebFluxConfigurer {
 
-    private fun naisAzureAdConfig(properties: EnvironmentProperties): OidcAuthenticatorConfig? {
-        return OidcAuthenticatorConfig()
-            .withDiscoveryUrl(properties.naisAadDiscoveryUrl)
-            .withClientId(properties.naisAadClientId)
-            .withUserRoleResolver(AzureAdUserRoleResolver())
+    private fun naisAzureAdConfig(properties: EnvironmentProperties): OidcAuthenticatorConfig {
+        return OidcAuthenticatorConfig(
+            discoveryUrl = properties.naisAadDiscoveryUrl,
+            clientIds = listOf(properties.naisAadClientId),
+            userRoleResolver = AzureAdUserRoleResolver()
+        )
     }
 
     @Bean
     fun customFilter(properties: EnvironmentProperties): WebFilter {
-        val authenticationFilter = OidcAuthenticationFilter(fromConfigs(naisAzureAdConfig(properties)))
-        return  ServletFilterToWebFilterAdapter(authenticationFilter)
-    }
-}
-
-
-/**
- * Adapter to convert a traditional javax.servlet.Filter into a reactive WebFlux WebFilter.
- */
-class ServletFilterToWebFilterAdapter(private val servletFilter: Filter) : WebFilter {
-
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        return Mono.create<Any>(Consumer { sink: MonoSink<Any> ->
-            try {
-                // Convert reactive request/response to servlet request/response
-                val httpRequest = exchange.request
-                val httpResponse = exchange.response
-
-                val servletRequest = getHttpServletRequest(httpRequest)
-                val servletResponse = getHttpServletResponse(httpResponse)
-
-                // Call the traditional Servlet Filter
-                servletFilter.doFilter(servletRequest, servletResponse, { req, res ->
-                    // After the filter is executed, continue with the reactive chain
-                    sink.success()
-                })
-            } catch (e: IOException) {
-                sink.error(e)
-            } catch (e: ServletException) {
-                sink.error(e)
-            }
-        }).then<Void>(chain.filter(exchange))
-    }
-
-    private fun getHttpServletRequest(request: ServerHttpRequest): HttpServletRequest {
-        return request as HttpServletRequest
-    }
-
-    private fun getHttpServletResponse(response: ServerHttpResponse): HttpServletResponse {
-        return response as HttpServletResponse
+        val naisAzureAdConfig = naisAzureAdConfig(properties)
+        val config = OidcAuthenticator.fromConfigs(naisAzureAdConfig)
+        val authenticationFilter = OicdAuthFilter(config)
+        return authenticationFilter
     }
 }
