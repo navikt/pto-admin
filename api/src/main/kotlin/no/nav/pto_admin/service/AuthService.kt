b60c7a01a78e849eea6d898fac532e84ff534a8a
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-13 08:42:15 +0100

    Cleanup

--- api/src/main/kotlin/no/nav/pto_admin/service/AuthService.kt
+++ api/src/main/kotlin/no/nav/pto_admin/service/AuthService.kt
@@ -2,13 +2,9 @@ package no.nav.pto_admin.service
 
 import com.nimbusds.jwt.JWTClaimsSet
 import no.nav.common.auth.context.AuthContext
-import no.nav.common.auth.context.AuthContextHolder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.util.context.ContextView
-import java.util.function.Function
-
-
 
 @Service
 class AuthService {
@@ -18,10 +14,6 @@ class AuthService {
             .mapNotNull { it.getStringClaim("NAVident") }
             .switchIfEmpty(Mono.error(RuntimeException("NAVident claim was null")))
     }
-
-	fun hentInnloggetBrukerAzureId(): Mono<String> {
-        return getLoggedInUserToken().map { it.subject }
-	}
 
     private fun getLoggedInUserToken(): Mono<JWTClaimsSet> {
         return Mono.deferContextual { ctx: ContextView ->
