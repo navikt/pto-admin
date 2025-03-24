36f812da8f70e70a9eb6c98a3fb2fa9c8f95bcc4
sigurdgroneng <sigurd.groneng@nav.no>
2025-03-24 13:14:22 +0100

    Register kotlin deserialization

--- api/src/main/kotlin/no/nav/pto_admin/pdl_pip/PdlPipClientImpl.kt
+++ api/src/main/kotlin/no/nav/pto_admin/pdl_pip/PdlPipClientImpl.kt
@@ -1,5 +1,6 @@
 package no.nav.pto_admin.pdl_pip
 
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.common.json.JsonUtils
 import no.nav.common.rest.client.RestClient
 import okhttp3.OkHttpClient
@@ -14,6 +15,10 @@ open class PdlPipClientImpl(
 ): PdlPipClient {
 
 	private val IDENT_PARAM_NAME: String = "ident"
+
+	init {
+		JsonUtils.getMapper().registerKotlinModule()
+	}
 
 	override fun hentBrukerInfo(
 		brukerIdent: String,
