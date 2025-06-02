1d64ba9af42ba8e6a94997297d46021f13ab8307
erikbosnes <erik.bosnes@nav.no>
2025-06-02 08:38:29 +0200

    Oppdater frontend for avslutt oppfølging for mange brukere

--- web-app/src/view/avsluttOppfølging/AvsluttOppfolging.tsx
+++ web-app/src/view/avsluttOppfølging/AvsluttOppfolging.tsx
@@ -1,7 +1,7 @@
 import { avsluttOppfolgingsperiode, batchAvsluttOppfolging } from '../../api';
 import React, { useState } from 'react';
 import { Card } from '../../component/card/card';
-import { Button, TextField } from '@navikt/ds-react';
+import { Button, Textarea, TextField } from '@navikt/ds-react';
 
 export function AvsluttOppfolging() {
 	return (
@@ -13,39 +13,54 @@ export function AvsluttOppfolging() {
 }
 
 function AvsluttOppfolgingForMangeBrukereCard() {
+	const [aktorIds, setAktorIds] = useState('');
+	const [begrunnelse, setBegrunnelse] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(undefined);
 
 	async function handleSubmit(e: any) {
 		e.preventDefault();
-		const { aktorIds: aktorIdStrings, begrunnelse } = Object.fromEntries(new FormData(e.target)) as unknown as {
-			aktorIds: string;
-			begrunnelse: string;
-		};
-		const aktorIds = aktorIdStrings.split(',').map(aktorId => aktorId.trim());
+		const aktorIdList = aktorIds.split(',').map(id => id.trim());
 		try {
 			setError(undefined);
 			setIsLoading(true);
-			await batchAvsluttOppfolging({ aktorIds, begrunnelse });
+			await batchAvsluttOppfolging({ aktorIds: aktorIdList, begrunnelse });
 		} catch (e: any) {
-			setError(e?.toString);
+			setError(e?.toString());
 		} finally {
 			setIsLoading(false);
 		}
 	}
 
 	return (
-		<div className="view kafka-admin">
-			<form onSubmit={handleSubmit} className="large-card card__content space-y-4">
-				<h1>Avslutt oppfølging for mange brukere</h1>
-				<label htmlFor="brukere">Liste over brukere som skal avsluttes:</label>
-				<textarea id="brukere" name="aktorIds" disabled={isLoading} />
-				<label htmlFor="begrunnelse">Begrunnelse for avsluttning av oppfølging:</label>
-				<input id="begrunnelse" type="text" name="begrunnelse" disabled={isLoading} />
-				<input type="submit" disabled={isLoading} />
-				{error || ''}
+		<Card
+			title="Avslutt oppfølging for mange brukere"
+			className="small-card"
+			innholdClassName="hovedside__card-innhold"
+		>
+			<form onSubmit={handleSubmit}>
+				<Textarea
+					label="AktørId-liste (kommaseparert)"
+					name="aktorIds"
+					value={aktorIds}
+					onChange={e => setAktorIds(e.target.value)}
+					disabled={isLoading}
+				/>
+				<TextField
+					label="Begrunnelse"
+					name="begrunnelse"
+					value={begrunnelse}
+					onChange={e => setBegrunnelse(e.target.value)}
+					disabled={isLoading}
+				/>
+
+				{error && <div className="error-message">{error}</div>}
+
+				<Button type="submit" disabled={isLoading}>
+					Avslutt oppfølging
+				</Button>
 			</form>
-		</div>
+		</Card>
 	);
 }
 
@@ -72,7 +87,7 @@ function AvsluttOppfolgingsperiode() {
 	}
 
 	return (
-		<Card title="Avslutt Oppfølgingsperiode" className="small-card" innholdClassName="hovedside__card-innhold">
+		<Card title="Avslutt en enkelt oppfølgingsperiode" className="small-card" innholdClassName="hovedside__card-innhold">
 			<form onSubmit={handleSubmit}>
 				<TextField label="AktørId" name="aktorId" value={aktorId} onChange={e => setAktorId(e.target.value)} />
 				<TextField
