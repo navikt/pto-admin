b5f8618ee3e9b7b7188732b55d2fdd0a582e3727
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-03-26 17:44:34 +0100

    Send logger til elastic

--- web-app/src/api/index.ts
+++ web-app/src/api/index.ts
@@ -104,6 +104,8 @@ export function republiserEndringPaaDialog(): AxiosPromise<JobId> {
 	return axiosInstance.post(`/api/admin/veilarbdialog/republiser/endring-paa-dialog`);
 }
 
+// Republisering veilarbarena
+
 export function republiserEndringPaaOppfolgingsbrukere(): AxiosPromise<JobId> {
 	return axiosInstance.post(`/api/admin/veilarbarena/republiser/endring-pa-bruker/all`);
 }
