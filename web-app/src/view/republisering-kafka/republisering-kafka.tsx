2e0ec9b711601dbd9893bf1f75eb16213c04100a
Tia Firing <tia.firing@nav.no>
2026-06-10 06:42:25 +0200

    info om å stoppe kafkalyttere før republisering av kontor for å unngå at vi sender utdatert informasjon

--- web-app/src/view/republisering-kafka/republisering-kafka.tsx
+++ web-app/src/view/republisering-kafka/republisering-kafka.tsx
@@ -66,7 +66,7 @@ export function RepubliseringKafka() {
 			/>
 			<RepubliseringsKort
 				tittel="Republiser arbeidsoppfølgingskontor endret"
-				beskrivelse="Republiserer kontoret til alle brukere med aktiv oppfølgingsperiode på topic dab.arbeidsoppfolgingskontortilordninger-v2"
+				beskrivelse="Republiserer kontoret til alle brukere med aktiv oppfølgingsperiode på topic dab.arbeidsoppfolgingskontortilordninger-v2. Husk å stoppe lytterne i ao-kontor med unleash-toggle ao-kontor.stopp-kafkalyttere først."
 				request={republiserArbeidsoppfolgingskontorendret}
 				topicNavn={'dab.arbeidsoppfolgingskontortilordninger-v2'}
 			/>
