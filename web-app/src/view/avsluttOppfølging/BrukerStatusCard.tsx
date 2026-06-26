e62ec3f37bde8c00232e9fd2ccec40afed37e7cf
Sigurd Grøneng <sigurdgroneng@gmail.com>
2026-06-26 15:09:09 +0200

    brukerstatus (#357)
    * Add brukerstatus tab
    
    * Replace deprecated tslint with eslint
    
    * Use pnpm in pipeline also
    
    * Add web-app dir to cache-path
    
    * Use same path style in cache paths
    
    * Add missing type packages
    
    * Bruk faner med resultat
    
    * Bedre result tabs
    
    * Use new type

--- web-app/src/view/avsluttOppfølging/BrukerStatusCard.tsx
+++ web-app/src/view/avsluttOppfølging/BrukerStatusCard.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Button, Heading, Loader, TextField } from '@navikt/ds-react';
+import { Button, Heading, TextField } from '@navikt/ds-react';
 import { Card } from '../../component/card/card';
 import { BooleanTag } from '../../component/BooleanTag';
 import { hentBrukerStatus, BrukerStatusDto } from '../../api/veilarboppfolging';
+import { UserQuery, UserQueryResultsContainer } from '../../component/UserQueryResults';
 
 export const BrukerStatusCard = () => {
-	const [brukerStatus, setBrukerStatus] = useState<BrukerStatusDto | null>(null);
+	const [results, setResults] = useState<UserQuery[]>([]);
 	const [error, setError] = useState<string | undefined>(undefined);
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -21,7 +22,15 @@ export const BrukerStatusCard = () => {
 		setIsLoading(true);
 		try {
 			const response = await hentBrukerStatus(fnr);
-			setBrukerStatus(response.data.brukerStatus);
+			const brukerStatus = response.data.brukerStatus;
+			setResults(prev => [
+				...prev,
+				{
+					id: prev.length + 1 + '',
+					ident: fnr,
+					component: <BrukerStatusResult brukerStatus={brukerStatus} />
+				}
+			]);
 		} catch (e: any) {
 			setError(e?.toString());
 		} finally {
@@ -34,61 +43,67 @@ export const BrukerStatusCard = () => {
 			<Heading size="medium">Brukerstatus</Heading>
 			<form className="space-y-4" onSubmit={fetchBrukerStatus}>
 				<TextField name="fnr" label="Fnr" />
-				<Button>Hent</Button>
+				<Button loading={isLoading} disabled={isLoading}>
+					Hent
+				</Button>
 			</form>
 
-			{isLoading && <Loader size="small" />}
 			{error && <div className="error-message">{error}</div>}
 
-			{brukerStatus && (
-				<div className="space-y-4">
-					<div className="bg-gray-100 p-3 border border-gray-200 rounded">
-						<div className="font-bold mb-2">Kontorsperre</div>
-						<div>
-							Er kontorsperret: <BooleanTag value={brukerStatus.erKontorsperret} />
-						</div>
-						{brukerStatus.kontorSperre && <div>Kontor ID: {brukerStatus.kontorSperre.kontorId}</div>}
-					</div>
-
-					<div className="bg-gray-100 p-3 border border-gray-200 rounded">
-						<div className="font-bold mb-2">KRR</div>
-						<div>
-							Reservert i KRR: <BooleanTag value={brukerStatus.krr.reservertIKrr} />
-						</div>
-						<div>
-							Kan varsles: <BooleanTag value={brukerStatus.krr.kanVarsles} />
-						</div>
-						<div>
-							Registrert i KRR: <BooleanTag value={brukerStatus.krr.registrertIKrr} />
-						</div>
-					</div>
-
-					<div className="bg-gray-100 p-3 border border-gray-200 rounded">
-						<div className="font-bold mb-2">Manuell</div>
-						{brukerStatus.manuell ? (
-							<>
-								<div>
-									Er manuell: <BooleanTag value={brukerStatus.manuell.erManuell ?? false} />
-								</div>
-								{brukerStatus.manuell.opprettetTidspunkt && (
-									<div>Opprettet tidspunkt: {brukerStatus.manuell.opprettetTidspunkt}</div>
-								)}
-								{brukerStatus.manuell.begrunnelse && (
-									<div>Begrunnelse: {brukerStatus.manuell.begrunnelse}</div>
-								)}
-								{brukerStatus.manuell.endretAvType && (
-									<div>Endret av type: {brukerStatus.manuell.endretAvType}</div>
-								)}
-								{brukerStatus.manuell.endretAvIdent && (
-									<div>Endret av ident: {brukerStatus.manuell.endretAvIdent}</div>
-								)}
-							</>
-						) : (
-							<div className="text-gray-500">Ingen manuell status</div>
-						)}
-					</div>
-				</div>
-			)}
+			<UserQueryResultsContainer
+				queries={results}
+				onCloseTab={itemId => setResults(prev => prev.filter(it => it.id !== itemId))}
+			/>
 		</Card>
 	);
 };
+
+function BrukerStatusResult({ brukerStatus }: { brukerStatus: BrukerStatusDto }) {
+	return (
+		<div className="space-y-4">
+			<div className="bg-gray-100 p-3 border border-gray-200 rounded">
+				<div className="font-bold mb-2">Kontorsperre</div>
+				<div>
+					Er kontorsperret: <BooleanTag value={brukerStatus.erKontorsperret} />
+				</div>
+				{brukerStatus.kontorSperre && <div>Kontor ID: {brukerStatus.kontorSperre.kontorId}</div>}
+			</div>
+
+			<div className="bg-gray-100 p-3 border border-gray-200 rounded">
+				<div className="font-bold mb-2">KRR</div>
+				<div>
+					Reservert i KRR: <BooleanTag value={brukerStatus.krr.reservertIKrr} />
+				</div>
+				<div>
+					Kan varsles: <BooleanTag value={brukerStatus.krr.kanVarsles} />
+				</div>
+				<div>
+					Registrert i KRR: <BooleanTag value={brukerStatus.krr.registrertIKrr} />
+				</div>
+			</div>
+
+			<div className="bg-gray-100 p-3 border border-gray-200 rounded">
+				<div className="font-bold mb-2">Manuell</div>
+				{brukerStatus.manuell ? (
+					<>
+						<div>
+							Er manuell: <BooleanTag value={brukerStatus.manuell.erManuell ?? false} />
+						</div>
+						{brukerStatus.manuell.opprettetTidspunkt && (
+							<div>Opprettet tidspunkt: {brukerStatus.manuell.opprettetTidspunkt}</div>
+						)}
+						{brukerStatus.manuell.begrunnelse && <div>Begrunnelse: {brukerStatus.manuell.begrunnelse}</div>}
+						{brukerStatus.manuell.endretAvType && (
+							<div>Endret av type: {brukerStatus.manuell.endretAvType}</div>
+						)}
+						{brukerStatus.manuell.endretAvIdent && (
+							<div>Endret av ident: {brukerStatus.manuell.endretAvIdent}</div>
+						)}
+					</>
+				) : (
+					<div className="text-gray-500">Ingen manuell status</div>
+				)}
+			</div>
+		</div>
+	);
+}
