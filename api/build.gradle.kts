8ecbc9c71f38f6541a17115394e5b60d0051e6fd
johannetronstad <johanne.tronstad@nav.no>
2026-07-08 12:36:39 +0200

    bump jvm

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,7 +5,7 @@
  */
 
 plugins {
-    kotlin("jvm") version "2.3.21"
+    kotlin("jvm") version "2.4.0"
     kotlin("plugin.spring") version "2.4.0"
     id("org.springframework.boot") version "4.1.0"
 }
