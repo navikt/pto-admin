1ce55537524ffe76f5fd1fb536c49f83482180b1
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-09-14 12:14:21 +0200

    Samler bumping pluss endringer for å fjerne feil

--- web-app/src/view/vedtaksstotte/vedtaksstotte-slett.tsx
+++ web-app/src/view/vedtaksstotte/vedtaksstotte-slett.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BodyShort, Button, TextField } from '@navikt/ds-react';
 import { Card } from '../../component/card/card';
 import { errorToast, successToast } from '../../utils/toast-utils';
+import { erGyldigFnr } from '../../utils/fnr-utils';
 import { slett14avedtak } from '../../api';
 import './vedtaksstotte.less';
 
@@ -16,8 +17,8 @@ export const VedtaksstotteSlett = () => {
 			errorToast('JournalpostId er ikke fylt inn');
 			return;
 		}
-		if (fnr.length !== 11) {
-			errorToast('Fødselsnummer har ikke 11 tegn');
+		if (!erGyldigFnr(fnr)) {
+			errorToast('Fødselsnummer er ikke gyldig');
 			return;
 		}
 		if (ansvarligVeileder.length !== 7) {
