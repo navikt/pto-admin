16618e3d4ca8eb22bda3f18a902cb7c13b609b2f
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-09-15 11:09:09 +0200

    Samler bumping

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,8 +5,8 @@
  */
 
 plugins {
-    kotlin("jvm") version "2.4.10"
-    kotlin("plugin.spring") version "2.4.10"
+    kotlin("jvm") version "2.4.20"
+    kotlin("plugin.spring") version "2.4.20"
     id("org.springframework.boot") version "4.1.1"
 }
 
