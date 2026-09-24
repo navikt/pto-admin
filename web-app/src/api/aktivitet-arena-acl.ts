07f070f3619d4e4784947eb818a2aa619e9086f4
sigurdgroneng <sigurd.groneng@nav.no>
2026-09-24 08:01:45 +0200

    Workaround the path rewrite

--- web-app/src/api/aktivitet-arena-acl.ts
+++ web-app/src/api/aktivitet-arena-acl.ts
@@ -3,7 +3,7 @@ import { fetchInstance } from './index';
 const springContextPath = '/aktivitet-arena-acl';
 const apiPath = '/api/admin';
 const poaoAdminRoutingPath = '/api/aktivitet-arena-acl';
-const aktivitetArenaAclAdminUrl = `${poaoAdminRoutingPath}${springContextPath}${apiPath}`;
+const aktivitetArenaAclAdminUrl = `${poaoAdminRoutingPath}${apiPath}${springContextPath}`;
 
 async function getJson<T>(path: string): Promise<{ data: T }> {
 	return fetchInstance.get<T>(`${aktivitetArenaAclAdminUrl}${path}`);
